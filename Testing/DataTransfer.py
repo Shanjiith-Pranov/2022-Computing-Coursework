6b966ac3a06b006d5b24b36542c3ec8ea58e1e92
Shanjiith <62892238+Shanjiith-Pranov@users.noreply.github.com>
2022-01-17 21:27:58 +0800

    Update DataTranfer

--- Testing/DataTransfer.py
+++ Testing/DataTransfer.py
@@ -1,28 +1,5 @@
-#Python code for connecting Arduino to Python
-#That's Engineering
-#29/04/2020
-
 import serial
-import time
 
+arduino = serial.Serial('/dev/cu.usbmodem141101', 115200, timeout=.1)
 
-def main_func():
-    arduino = serial.Serial('/dev/cu.usbmodem141101', 9600) #Established serial connection to Arduino
-    arduino_data = arduino.read()
-    arduino_data2 = arduino.read()
-    print(arduino_data)
-    print(arduino_data2)
-
-    #arduino.close()#Connection closed
-    
-
-
-# ----------------------------------------Main Code------------------------------------
-# Declare variables to be used
-
-
-print('Program started')
-
-
-while True:
-    main_func()
+while True:data = arduino.read();print(data)
