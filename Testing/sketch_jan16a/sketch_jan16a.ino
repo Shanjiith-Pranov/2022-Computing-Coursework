217e06b137549d8b3c85a766afae2e0874b7abfd
Shanjiith <62892238+Shanjiith-Pranov@users.noreply.github.com>
2022-01-17 21:34:49 +0800

    Update Arduino code

--- Testing/sketch_jan16a/sketch_jan16a.ino
+++ Testing/sketch_jan16a/sketch_jan16a.ino
@@ -2,21 +2,20 @@
 //That's Engineeing
 //29/04/2020
 
-int a;
 
 void setup()
 {
   Serial.begin(115200);  
+  pinMode(12, INPUT);
 }
 
 void loop()
 {
-    Serial.println("1");
-    delay(40);
-    Serial.println("2");
+  if (digitalRead(12) == HIGH){
+    Serial.println("On");
+  } else {
+    Serial.println("Off");
+  }
+  
     delay(40);
 }
-
-
-  
-  
