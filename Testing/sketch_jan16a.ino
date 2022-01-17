2f0f180a08c5c220b37975eca525ea40ec0d7425
Shanjiith <62892238+Shanjiith-Pranov@users.noreply.github.com>
2022-01-17 21:27:46 +0800

    Create sketch_jan16a.ino

---
+++ Testing/sketch_jan16a.ino
@@ -0,0 +1,22 @@
+//Arduino code for sending DHT11 data to python
+//That's Engineeing
+//29/04/2020
+
+int a;
+
+void setup()
+{
+  Serial.begin(115200);  
+}
+
+void loop()
+{
+    Serial.println("1");
+    delay(40);
+    Serial.println("2");
+    delay(40);
+}
+
+
+  
+  
