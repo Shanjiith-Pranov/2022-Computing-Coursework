f5f7dcd26f4ee45e05b97fd40e5a2e020aa07d10
Shanjiith <62892238+Shanjiith-Pranov@users.noreply.github.com>
2022-01-17 21:37:50 +0800

    Update DataTransfer.py

--- Testing/DataTransfer.py
+++ Testing/DataTransfer.py
@@ -1,5 +1,7 @@
 import serial
 
-arduino = serial.Serial('/dev/cu.usbmodem141101', 115200, timeout=.1)
+arduino = serial.Serial('/dev/cu.usbmodem141101', 115200, timeout=.1) #initialize connection with arduino
 
-while True:data = arduino.read();print(data)
+while True:
+    data = arduino.read() #Read the data received from the arduino
+    print(data)
