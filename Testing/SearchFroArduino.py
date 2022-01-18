511a78d4f994e46f1e99248fbcea608da06e5d66
Shanjiith <62892238+Shanjiith-Pranov@users.noreply.github.com>
2022-01-18 14:21:37 +0800

    Update SearchFroArduino.py

--- Testing/SearchFroArduino.py
+++ Testing/SearchFroArduino.py
@@ -1,10 +1,12 @@
 import serial
 import serial.tools.list_ports
 
+# IOUSBHostDevice is the arduino's tag or something, the serial value changes for each mac
+
 arduino_ports = [
     p.device
     for p in serial.tools.list_ports.comports()
-    if 'IOUSBHostDevice' in p.description  # may need tweaking to match new arduinos
+    if 'IOUSBHostDevice' in p.description 
 ]
 if not arduino_ports:
     print("No Arduino found")
@@ -14,3 +16,4 @@ if len(arduino_ports) > 1:
 ser = serial.Serial(arduino_ports[0])
 
 ##  /dev/cu.usbmodem141101 - IOUSBHostDevice
+
