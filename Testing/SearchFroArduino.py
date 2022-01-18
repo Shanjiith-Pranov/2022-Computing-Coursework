c19dc7839722864076cec3c7e59d4ed48acc339d
Shanjiith <62892238+Shanjiith-Pranov@users.noreply.github.com>
2022-01-18 21:39:27 +0800

    Update - Loop until arduino is found

--- Testing/SearchFroArduino.py
+++ Testing/SearchFroArduino.py
@@ -1,19 +1,23 @@
 import serial
 import serial.tools.list_ports
+import time
 
 # IOUSBHostDevice is the arduino's tag or something, the serial value changes for each mac
+while True:
+    arduino_ports = [
+        p.device
+        for p in serial.tools.list_ports.comports()
+        if 'IOUSBHostDevice' in p.description 
+    ]
+    if not arduino_ports:
+        print("No Arduino found")
+        continue
+    if len(arduino_ports) > 1:
+        print('Multiple Arduinos found - using the first')
 
-arduino_ports = [
-    p.device
-    for p in serial.tools.list_ports.comports()
-    if 'IOUSBHostDevice' in p.description 
-]
-if not arduino_ports:
-    print("No Arduino found")
-if len(arduino_ports) > 1:
-    print('Multiple Arduinos found - using the first')
+    ser = serial.Serial(arduino_ports[0])
+    print("Arduino found")
+    break
 
-ser = serial.Serial(arduino_ports[0])
-
-##  /dev/cu.usbmodem141101 - IOUSBHostDevice
+    ##  /dev/cu.usbmodem141101 - IOUSBHostDevice
 
