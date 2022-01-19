49cd825375018074c30f74924a1c2d851c7607da
Shanjiith <62892238+Shanjiith-Pranov@users.noreply.github.com>
2022-01-19 23:03:22 +0800

    Update WASDEmulation.ino

---
+++ Testing/Level 1/WASDEmulation/WASDEmulation.ino
@@ -0,0 +1,22 @@
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(13, INPUT);
+  pinMode(12, INPUT);
+  pinMode(13, INPUT);
+  pinMode(10, INPUT);
+}
+
+void loop()
+{
+  if (digitalRead(13) == HIGH){
+    Serial.println("W");
+  }else if (digitalRead(12) == HIGH){
+    Serial.println("A");
+  }else if (digitalRead(11) == HIGH){
+    Serial.println("S");
+  }else if (digitalRead(10) == HIGH){
+    Serial.println("D");
+  }else{
+  }
+}
