b6348ec080f1895862f1a570b677466d1ff47ce2
Shanjiith <62892238+Shanjiith-Pranov@users.noreply.github.com>
2022-01-20 14:24:51 +0800

    Update WASDEmulation.ino

--- Testing/Level 1/WASDEmulation/WASDEmulation.ino
+++ Testing/Level 1/WASDEmulation/WASDEmulation.ino
@@ -1,23 +1,30 @@
+String pressedKeys;
+
 void setup()
 {
   Serial.begin(115200);
-  pinMode(13, INPUT); #W
-  pinMode(12, INPUT); #A
-  pinMode(13, INPUT); #S
-  pinMode(10, INPUT); #D
+  pinMode(13, INPUT); //W
+  pinMode(12, INPUT); //A
+  pinMode(13, INPUT); //S
+  pinMode(10, INPUT); //D
 }
 
 void loop()
 {
+
   if (digitalRead(13) == HIGH){
-    Serial.println("w");
-  }else if (digitalRead(12) == HIGH){
-    Serial.println("a");
-  }else if (digitalRead(11) == HIGH){
-    Serial.println("s");
-  }else if (digitalRead(10) == HIGH){
-    Serial.println("d");
-  }else{
-      continue
+    pressedKeys = pressedKeys + "w "
   }
+  if (digitalRead(12) == HIGH){
+    pressedKeys = pressedKeys + "a "
+  }
+  if (digitalRead(11) == HIGH){
+    pressedKeys = pressedKeys + "s "
+  }
+  if (digitalRead(10) == HIGH){
+    pressedKeys = pressedKeys + "d "
+  }
+  Serial.println(pressedKeys);
+  pressedKeys = "";
+
 }
