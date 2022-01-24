39b1f15d8277761760e9b1bb0867718dbf3f5d1c
Shanjiith <62892238+Shanjiith-Pranov@users.noreply.github.com>
2022-01-24 15:31:03 +0800

    Update Port+Data+Emulation.py

--- Testing/Level 2/Port+Data+Emulation.py
+++ Testing/Level 2/Port+Data+Emulation.py
@@ -1,3 +1,4 @@
+from tkinter import TRUE
 import serial
 import serial.tools.list_ports
 import pyautogui
@@ -7,8 +8,12 @@ keys = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r",
 selectedKeys = ["w","a","s","d"] # the keys that are currently selected, cuztomisable list
 
 pressedKey = ''
+arduinoFound = False
+placeholder = newdata = ""
 
-while True:
+
+t_end = time.time() + 10
+while time.time() < t_end:
     arduino_ports = [ # Find all the ports with "IOUSBHostDevice" as its tag
         p.device
         for p in serial.tools.list_ports.comports()
@@ -16,24 +21,29 @@ while True:
     ]
     if not arduino_ports: #Loops until arduino is found
         continue
-    if len(arduino_ports) > 1:
-        print('Multiple Arduinos found - using the first')
+    else:        
+        if len(arduino_ports) > 1:
+            print('Multiple Arduinos found - using the first')
+        else:
+            print("Arduino found") # Arduino is found
+        arduinoFound = True
+        arduino = serial.Serial(arduino_ports[0], 115200, timeout=.1) #initialize connection with arduino
+        break #Break out of the loop once the arduino is found 
+   
+if arduinoFound:
+    while True:
+        newdata = ""
+        data = (arduino.readline()).split(" ") #Read the data receiveda from the arduino
+        del data[-1]
+        for i in range(len(data)):
+            data[i] = int(data[i]) #converts every value into int
+            newdata = newdata + selectedKeys[data[i]] #converts into keypress + appends into a string
+        if placeholder != newdata:
+            pyautogui.keyUp(placeholder) #keyup if no input detected + if placeholder has a previous input
+            print("up: " + placeholder + "\n")
+            pyautogui.keyDown(newdata) #keyup ssdddddif no input detected + if placeholder has a previous input
+            print("down: " + newdata + "\n")
+            placeholder = newdata
+    else:
+        print("Arduino not found")
 
-    print("Arduino found") # Arduino is found
-    arduino = serial.Serial(arduino_ports[0], 115200, timeout=.1) #initialize connection with arduino
-    break #Break out of the loop once the arduino is found
-
-placeholder = newdata = ""
-while True:
-    newdata = ""
-    data = (arduino.readline()).split(" ") #Read the data receiveda from the arduino
-    del data[-1]
-    for i in range(len(data)):
-        data[i] = int(data[i]) #converts every value into int
-        newdata = newdata + selectedKeys[data[i]] #converts into keypress + appends into a string
-    if placeholder != newdata:
-        pyautogui.keyUp(placeholder) #keyup if no input detected + if placeholder has a previous input
-        print("up: " + placeholder + "\n")
-        pyautogui.keyDown(newdata) #keyup ssdddddif no input detected + if placeholder has a previous input
-        print("down: " + newdata + "\n")
-        placeholder = newdata
