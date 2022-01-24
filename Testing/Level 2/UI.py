4ebb93bd98afa81b937b232f814ed942ee224c0d
Shanjiith <62892238+Shanjiith-Pranov@users.noreply.github.com>
2022-01-24 19:52:58 +0800

    Update UI.py

--- Testing/Level 2/UI.py
+++ Testing/Level 2/UI.py
@@ -1,1 +1,36 @@
-import tk
+import tkinter as tk
+
+KEYS = ["w","a","s","d"]
+window = tk.Tk()
+window.mainloop()
+
+tk.Label(text="Control Menu")
+tk.pack()
+Button1 = tk.Entry()
+Button1.pack()
+Button1TRUE = Button1.get()
+
+
+'''
+from tkinter import *
+from tkinter import ttk
+root = Tk()
+frm = ttk.Frame(root, padding=10)
+frm.grid()
+ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
+ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
+root.mainloop()
+
+#diff
+
+master = Tk()
+
+variable = StringVar(master)
+variable.set("one") # default value
+
+w = OptionMenu(master, variable, "one", "two", "three")
+w.pack()
+
+mainloop()
+'''
+#Learn Dropdown window
