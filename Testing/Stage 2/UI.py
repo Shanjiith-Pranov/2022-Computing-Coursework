c9603ecc50f1aa1b89abb10e81a57275b9663099
Shanjiith <62892238+Shanjiith-Pranov@users.noreply.github.com>
2022-01-31 18:55:56 +0800

    Changed the files

--- Testing/Stage 2/UI.py
+++ Testing/Stage 2/UI.py
@@ -1,41 +1,42 @@
-import tkinter as tk
+import tkinter 
 
-def sendKeys(keys): #This functions allows the sending of data to the other .py file
-    return keys
+keys = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","1","2","3","4","5","6","7","8","9","0","[","]","\\",";","'",",",".","/","up","down","left","right"] #The list of all possible keys
+selectedKeys = ["w","a","s","d"] # the keys that are currently selected, cuztomisable list
+root = tkinter.Tk()
+root.title('Enter your Keys ')
 
-KEYS = ["w","a","s","d"] #Make this cuztomisable
-window = tk.Tk()
-window.mainloop()
 
-tk.Label(text="Control Menu")
-tk.pack()
-Button1 = tk.Entry()
-Button1.pack()
-Button1TRUE = Button1.get()
 
-### WHEN SAVE BUTTON IS PRESSED
-sendKeys(KEYS)
+# Dropdown window
+clicked1 = tkinter.StringVar()#Change to String
+clicked1.set('w') #Default Key1
+clicked2 = tkinter.StringVar()
+clicked2.set('w') #Default Key2
+clicked3 = tkinter.StringVar()
+clicked3.set('w') #Default Key3
+clicked4 = tkinter.StringVar()
+clicked4.set('w') #Default Key4
 
-'''
-from tkinter import *
-from tkinter import ttk
-root = Tk()
-frm = ttk.Frame(root, padding=10)
-frm.grid()
-ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
-ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
-root.mainloop()
 
-#diff
+drop1 = tkinter.OptionMenu(root,clicked1,*keys) #Drop Down Menu for button1
+drop1.grid(row=0,column=1)
+Label1 = tkinter.Label(root, text="First Button") #Show what is button1
+Label1.grid(row=0,column=0)
 
-master = Tk()
+drop2 = tkinter.OptionMenu(root,clicked2,*keys) #Drop Down Menu for button2
+drop2.grid(row=1,column=1)
+Label2 = tkinter.Label(root, text="Second Button") #Show what is button2
+Label2.grid(row=1,column=0)
 
-variable = StringVar(master)
-variable.set("one") # default value
+drop3 = tkinter.OptionMenu(root,clicked3,*keys) #Drop Down Menu for button3
+drop3.grid(row=2,column=1)
+Label3 = tkinter.Label(root, text="Third Button") #Show what is button3
+Label3.grid(row=2,column=0)
 
-w = OptionMenu(master, variable, "one", "two", "three")
-w.pack()
+drop4 = tkinter.OptionMenu(root,clicked4,*keys) #Drop Down Menu for button4
+drop4.grid(row=3,column=1)
+Label4 = tkinter.Label(root, text="Fourth Button") #Show what is button4
+Label4.grid(row=3,column=0)
 
-mainloop()
-'''
-#Learn Dropdown window
+root.mainloop() #Loop forever/stay in window
+
