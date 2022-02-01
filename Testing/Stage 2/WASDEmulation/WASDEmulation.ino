0ecd2f9ab07576fbcf58d3baac44bda57ed3990b
Shanjiith <62892238+Shanjiith-Pranov@users.noreply.github.com>
2022-02-01 15:24:19 +0800

    Update WASDEmulation.ino

--- Testing/Stage 2/WASDEmulation/WASDEmulation.ino
+++ Testing/Stage 2/WASDEmulation/WASDEmulation.ino
@@ -1,21 +1,25 @@
 String pressedKeys;
-int button1 = 13; //W
-int button2 = 12; //A
-int button3 = 11; //S
-int enable3 = 8;  //S - Enable
+int button1 = 13; // W
+int enable1 = 8;  // W   - Enable
+int button2 = 12; // A
+int enable2 = 7;  // A   - Enable
+int button3 = 11; // S
+int enable3 = 6;  // S   - Enable
 int button4 = 10; // D
+int enable4 = 5;  // D   - Enable
 int button5 = 9;  // Esc
+int enable5 = 4;  // Esc - Enable
 int enable[] = {1,1,1,1,1};
 
 void setup()
 {
   Serial.begin(115200);
-  pinMode(button1, INPUT); //W
-  pinMode(button2, INPUT); //A
-  pinMode(button3, INPUT); //S
-  pinMode(enable3, INPUT); //S enable
-  pinMode(button4, INPUT); //D
-  pinMode(button5, INPUT); //esc
+  pinMode(button1, INPUT);
+  pinMode(button2, INPUT);
+  pinMode(button3, INPUT);
+  pinMode(enable3, INPUT);
+  pinMode(button4, INPUT);
+  pinMode(button5, INPUT);
 }
 
 void loop()
@@ -38,6 +42,8 @@ void loop()
   if (digitalRead(button5) == LOW){
     pressedKeys = pressedKeys + "4 ";
   }
+  
+
   Serial.println(pressedKeys);
   pressedKeys = "";
   delay(10);
