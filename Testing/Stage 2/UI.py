bcceac8ab13ec40281c842003111068385d1d462
Shanjiith <62892238+Shanjiith-Pranov@users.noreply.github.com>
2022-02-03 14:28:26 +0800

    Rename

--- Testing/Stage 2/UI.py
+++ Testing/Stage 2/UI.py
@@ -1,4 +1,5 @@
 import tkinter 
+import Port\+Data\+Emulation
 
 keys = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","1","2","3","4","5","6","7","8","9","0","[","]","\\",";","'",",",".","/","up","down","left","right"] #The list of all possible keys
 selectedKeys = ["w","a","s","d"] # the keys that are currently selected, cuztomisable list
