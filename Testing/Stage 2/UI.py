b9b1e464ce5c8926f89a5c4056fe8ade4c452298
dariusKoh <55723854+dariusKoh@users.noreply.github.com>
2022-02-05 14:43:38 +0800

    Update UI.py

--- Testing/Stage 2/UI.py
+++ Testing/Stage 2/UI.py
@@ -1,15 +1,25 @@
 import tkinter 
 #import PortDataEmulation
-i = True
+
 keys = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","1","2","3","4","5","6","7","8","9","0","[","]","\\",";","'",",",".","/","up","down","left","right"] #The list of all possible keys
 selectedKeys = ["w","a","s","d","esc"] # the keys that are currently selected, cuztomisable list
+
 root = tkinter.Tk()
-root.title('Enter your Keys ')
+root.title('Enter your Keys')
+
 def open():
     top = tkinter.Toplevel() #Open New Window
     top.title("Syncing the Device")
+
 def show():
-    ChoiceLabel = tkinter.Label(root, text=selectedKeys).pack()
+    selectedKeys[0] = clicked1.get()
+    selectedKeys[1] = clicked2.get()
+    selectedKeys[2] = clicked3.get()
+    selectedKeys[3] = clicked4.get()
+    selectedKeys[4] = clicked5.get()
+
+    ChoiceLabel = tkinter.Label(root, text=selectedKeys)
+    ChoiceLabel.grid(row=4,column=3)
 
 #enable = PortDataEmulation.sendEnable() #[X,X,X,X,X] where X is 1/0
 # Dropdown window
@@ -27,43 +37,36 @@ clicked5.set('esc') #Default Key4
 WindowButton = tkinter.Button(root, text="Sync the Device",command=open) #Btn to open window
 WindowButton.grid(row=0,column=3)
 
-
+ChoiceLabel = tkinter.Label(root, text=selectedKeys)
+ChoiceLabel.grid(row=4,column=3)
 
 drop1 = tkinter.OptionMenu(root,clicked1,*keys) #Drop Down Menu for button1
 drop1.grid(row=0,column=1)
-Label1 = tkinter.Label(root, text="First Button") #Show what is button1
-Label1.grid(row=0,column=0)
+btnLabel1 = tkinter.Label(root, text="First Button") #Show what is button1
+btnLabel1.grid(row=0,column=0)
 
 drop2 = tkinter.OptionMenu(root,clicked2,*keys) #Drop Down Menu for button2
 drop2.grid(row=1,column=1)
-Label2 = tkinter.Label(root, text="Second Button") #Show what is button2
-Label2.grid(row=1,column=0)
+btnLabel2 = tkinter.Label(root, text="Second Button") #Show what is button2
+btnLabel2.grid(row=1,column=0)
 
 drop3 = tkinter.OptionMenu(root,clicked3,*keys) #Drop Down Menu for button3
 drop3.grid(row=2,column=1)
-Label3 = tkinter.Label(root, text="Third Button") #Show what is button3
-Label3.grid(row=2,column=0)
+btnLabel3 = tkinter.Label(root, text="Third Button") #Show what is button3
+btnLabel3.grid(row=2,column=0)
 
 drop4 = tkinter.OptionMenu(root,clicked4,*keys) #Drop Down Menu for button4
 drop4.grid(row=3,column=1)
-Label4 = tkinter.Label(root, text="Fourth Button") #Show what is button4
-Label4.grid(row=3,column=0)
+btnLabel4 = tkinter.Label(root, text="Fourth Button") #Show what is button4
+btnLabel4.grid(row=3,column=0)
 
 drop5 = tkinter.OptionMenu(root,clicked5,*keys) #Drop Down Menu for button4
 drop5.grid(row=4,column=1)
-Label5 = tkinter.Label(root, text="Fifth Button") #Show what is button4
-Label5.grid(row=4,column=0)
+btnLabel5 = tkinter.Label(root, text="Fifth Button") #Show what is button4
+btnLabel5.grid(row=4,column=0)
 
 ChoiceButton = tkinter.Button(root, text="Confirm selection",command=show) #Btn to set the selectedKeys
 ChoiceButton.grid(row=5,column=0)
-while i == True:
-    choice1 = clicked1.get()
-    choice2 = clicked2.get()
-    choice3 = clicked3.get()
-    choice4 = clicked4.get()
-    choice5 = clicked5.get()
-    print(choice1,choice2,choice3,choice4,choice5)
-    break
 
 root.mainloop() #Loop forever/stay in window
 
