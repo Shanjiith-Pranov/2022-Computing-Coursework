103e4077c707e133940aeae1690e56b4a3658aa7
dariusKoh <55723854+dariusKoh@users.noreply.github.com>
2022-02-05 14:57:36 +0800

    Cleaning up the code

--- Testing/Stage 2/UI.py
+++ Testing/Stage 2/UI.py
@@ -19,8 +19,8 @@ def show(): #updates the selected keys when Confirm Selection is pressed
     selectedKeys[3] = clicked4.get()
     selectedKeys[4] = clicked5.get()
     #updates the label every time Confirm Selection is pressed
-    ChoiceLabel = tkinter.Label(root, text=selectedKeys) #shows all the currently selected keys
-    ChoiceLabel.grid(row=4,column=3)
+    showChoices = tkinter.Label(root, text=selectedKeys) #shows all the currently selected keys
+    showChoices.grid(row=4,column=3)
 
 #enable = PortDataEmulation.sendEnable() #[X,X,X,X,X] where X is 1/0
 
@@ -36,40 +36,40 @@ clicked4.set('d') #Default Key4
 clicked5 = tkinter.StringVar()
 clicked5.set('esc') #Default Key4
 
-WindowButton = tkinter.Button(root, text="Sync the Device",command=open) #Btn to open window to sync to arduino
-WindowButton.grid(row=0,column=3)
+windowButton = tkinter.Button(root, text="Sync the Device",command=open) #Btn to open window to sync to arduino
+windowButton.grid(row=0,column=3)
 
-ChoiceLabel = tkinter.Label(root, text=selectedKeys) #shows all the currently selected keys
-ChoiceLabel.grid(row=4,column=3)
+choiceLabel = tkinter.Label(root, text="Selected keys:") #shows what showChoices means
+showChoices = tkinter.Label(root, text=selectedKeys) #shows all the currently selected keys
+choiceLabel.grid(row=3,column=3)
+showChoices.grid(row=4,column=3)
 
 drop1 = tkinter.OptionMenu(root,clicked1,*keys) #Drop Down Menu for button1
-drop1.grid(row=0,column=1)
 btnLabel1 = tkinter.Label(root, text="First Button") #Show what is button1
+drop1.grid(row=0,column=1)
 btnLabel1.grid(row=0,column=0)
 
 drop2 = tkinter.OptionMenu(root,clicked2,*keys) #Drop Down Menu for button2
-drop2.grid(row=1,column=1)
 btnLabel2 = tkinter.Label(root, text="Second Button") #Show what is button2
+drop2.grid(row=1,column=1)
 btnLabel2.grid(row=1,column=0)
 
 drop3 = tkinter.OptionMenu(root,clicked3,*keys) #Drop Down Menu for button3
-drop3.grid(row=2,column=1)
 btnLabel3 = tkinter.Label(root, text="Third Button") #Show what is button3
+drop3.grid(row=2,column=1)
 btnLabel3.grid(row=2,column=0)
 
 drop4 = tkinter.OptionMenu(root,clicked4,*keys) #Drop Down Menu for button4
-drop4.grid(row=3,column=1)
 btnLabel4 = tkinter.Label(root, text="Fourth Button") #Show what is button4
+drop4.grid(row=3,column=1)
 btnLabel4.grid(row=3,column=0)
 
-drop5 = tkinter.OptionMenu(root,clicked5,*keys) #Drop Down Menu for button4
+drop5 = tkinter.OptionMenu(root,clicked5,*keys) #Drop Down Menu for button5
+btnLabel5 = tkinter.Label(root, text="Fifth Button") #Show what is button5
 drop5.grid(row=4,column=1)
-btnLabel5 = tkinter.Label(root, text="Fifth Button") #Show what is button4
 btnLabel5.grid(row=4,column=0)
 
-ChoiceButton = tkinter.Button(root, text="Confirm selection",command=show) #Btn to set the selectedKeys
-ChoiceButton.grid(row=5,column=0)
+choiceButton = tkinter.Button(root, text="Confirm selection",command=show) #Btn to set the selectedKeys
+choiceButton.grid(row=5,column=0)
 
 root.mainloop() #Loop forever/stay in window
-
-
